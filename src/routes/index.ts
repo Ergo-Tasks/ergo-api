96aca755b874514dd92c3e85bc0d953080288453
Kieran Vieira <kieranvieira@live.com>
2021-07-29 17:02:58 -0400

    Added docstrings for applyRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,11 @@ import { Express } from "express";
 
 import userRoute from "./users";
 
+/**
+ * Handles applying all routes to the given server
+ * 
+ * @param server - Server to add route handlers to
+ */
 const applyRoutes = (server: Express) => {
   server.use("/api/users", userRoute);
   server.get("/", (req, res) => {res.send("Ergo api running here")});
