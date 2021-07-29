56ac2b728a6863694a6b2a32bca781d12647c970
Kieran Vieira <kieranvieira@live.com>
2021-07-28 23:50:14 -0400

    Added middleware bootstrap

---
+++ src/middleware/index.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+/**
+ * Handles applying custom and imported middleware
+ * to the server passed as an argument 
+ * 
+ * @param server - Express server to use middleware on
+ */
+const applyMiddleware = (server: Express) => {
+  server.use(helmet());
+  server.use(express.json());
+  server.use(cors());
+  server.use(morgan("tiny"));
+}
+
+export default applyMiddleware
