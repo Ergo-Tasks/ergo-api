f4d107d056fd45c87bd7fdb18460daf5eace6282
Kayden Grant <kayden@Kaydens-MacBook-Pro.local>
2021-08-03 21:40:37 -0700

    creating post endpoint that returns auth token based on user email and password

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,7 @@ import { NextFunction, Router, Request, Response } from "express";
 import { User } from "../typeorm/entities/User"
 import bcrypt from "bcrypt"
 import { getRepository } from "typeorm";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -39,6 +40,37 @@ router.post('/', async (req, res) => {
 
 });
 
+//creating post endpoint to return auth token
+router.post('/login', async (req, res) => {
 
+  //try-catch for error handling and for await functionality
+  try {
+
+    //accessing request contents
+    const body: User = req.body;
+    //finds entity that matches user email sent through the request
+    const user = await User.findOneOrFail({
+      email: body.email
+    })
+    
+    //returns true if request password matches hashed password in database
+    if (bcrypt.compareSync(body.password, user.password)) {
+      //creating payload for jwt - storing unique unsensitive user data in token for verification later
+      const payload = {
+        "email": user.email,
+        "id": user.id
+      }
+      //accessing secret from local env and creating jwt token
+      const secret = process.env.JWT_SECRET || "default";      
+      const token = jwt.sign(payload, secret);
+      res.status(200).json({token});
+    } else {
+      throw new Error;
+    }
+    
+  } catch (err) {
+    res.status(401).json({message: "Bad request"});
+  }
+});
 
 export default router;
