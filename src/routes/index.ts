80f7acacc2d0c075a35e696477af1766fd4c9c40
Reeves <reeves.abbas@gmail.com>
2021-08-09 21:11:26 -0400

    put Kieran's comment back

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,12 @@ import { Express } from "express";
 
 import userRoute from "./users";
 
+/** 
+ * Handles applying all routes to given server
+ * 
+ * @param server - Server to add route handlers to
+ */
+
 const applyRoutes = (server: Express) => {
   server.use("/api/users", userRoute);
   server.get("/", (req, res) => {res.send("Ergo api running here")});
