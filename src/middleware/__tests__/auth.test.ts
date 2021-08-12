3dc0f0ebae83c9a0767d5c702e5b7c7ece00d27d
Kayden Grant <kayden@Kaydens-MacBook-Pro.local>
2021-08-12 11:00:39 -0700

    added first test for auth middleware

--- src/middleware/__tests__/auth.test.ts
+++ src/middleware/__tests__/auth.test.ts
@@ -1,11 +1,31 @@
-import supertest from 'supertest'
+import { NextFunction, Request, Response, Router } from "express";
 
 import createConnection from "../../typeorm";
 import server from "../../server";
 import { User } from "../../typeorm/entities/User";
-
-const request = supertest(server);
+import { restricted } from "../../middleware/auth"
 
 describe('Token authentication middleware', () => {
   
-})
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
+
+  beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {json: jest.fn()}
+    nextFunction = jest.fn()
+  });
+
+  it('Should call next function', () => {
+    mockRequest = {
+      headers: {authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthZ3JhbnRAdXcuZWR1MiIsImlkIjoiNWU0NmU4MGYtNWE1Ni00MWY1LTk4NzctOTQ0NjFiODkxNWNmIiwiaWF0IjoxNjI4NTQzNDEzfQ.4qY27QJ_I9Ds2iy9ouC_NIrIdtz-WuqaoSyji3KZruU"},
+      params: {userId: "5e46e80f-5a56-41f5-9877-94461b8915cf"}
+    }
+    restricted(mockRequest as Request, mockResponse as Response, nextFunction);
+    expect(nextFunction).toBeCalled();
+  });
+
+});
+
+//mockResponse.json toBe
