d6969f702a6eb3109a92fff375c2769fbc93f8e0
reevesabbas <reeves.abbas@gmail.com>
2021-08-16 21:19:44 -0400

    finished all error tests, implemented way to retrieve status and json from middleware

--- src/middleware/__tests__/auth.test.ts
+++ src/middleware/__tests__/auth.test.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { NextFunction, Request, response, Response } from "express";
 
 import createConnection from "../../typeorm";
 import server from "../../server";
@@ -7,25 +7,103 @@ import { restricted } from "../../middleware/auth"
 
 describe('Token authentication middleware', () => {
   
+  /**
+   * Setting the mock variable's types to Partial<> will return all properties
+   * on that type, but they are all optional.
+   * 
+   * jest.fn() is a mock function used to mimick the environment our test function is
+   * being implemented on.
+   * 
+   * 2 result variables for capturing response fields (auth.ts sends a status and json body)
+   */
   let mockRequest: Partial<Request>;
   let mockResponse: Partial<Response>;
   let nextFunction: NextFunction = jest.fn();
+  let resultJson = {};
+  let resultStatus = {};
 
+
+  /**
+   * jest function to reset variables before each test,
+   * 
+   * Express methods are being caught from middlware, then runs the callback function to handle
+   * how the variables given to us by express are stored in this test. 
+   * Returns the response object in order to 'chain' the methods as they are in the controller. (.status().json())
+   */
   beforeEach(() => {
+
     mockRequest = {};
-    mockResponse = {json: jest.fn()}
-    nextFunction = jest.fn()
+    mockResponse = {};
+    nextFunction = jest.fn();
+    resultJson = {};
+    resultStatus = {};
+
+    //'result' is recieved from what is being passed in to '.status' and '.json' when called in middleware.
+    mockResponse.status = jest.fn().mockImplementation((result) => {
+      resultStatus = result;
+      return mockResponse;
+    })
+
+    mockResponse.json = jest.fn().mockImplementation((result) => {
+      resultJson = result;
+      return mockResponse;
+    })
+
   });
 
   it('Should call next function', () => {
+
     mockRequest = {
-      headers: {authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthZ3JhbnRAdXcuZWR1MiIsImlkIjoiNWU0NmU4MGYtNWE1Ni00MWY1LTk4NzctOTQ0NjFiODkxNWNmIiwiaWF0IjoxNjI4NTQzNDEzfQ.4qY27QJ_I9Ds2iy9ouC_NIrIdtz-WuqaoSyji3KZruU"},
-      params: {userId: "5e46e80f-5a56-41f5-9877-94461b8915cf"}
-    }
+      headers: {authorization: 'valid'},
+      params: {userId: '1'}
+  }
+    
     restricted(mockRequest as Request, mockResponse as Response, nextFunction);
+
     expect(nextFunction).toBeCalled();
+
   });
 
-});
+  it('Should call status 400 due to token unable to be parsed', () => {
+    
+    //Auth token is not correct format (missing 1 character).
+    mockRequest = {
+      headers: {authorization: 'invalid'},
+      params: {userId: '2'}
+  }
+    
+    restricted(mockRequest as Request, mockResponse as Response, nextFunction);
+    
+    expect(resultStatus).toBe(400);
+    expect(resultJson).toEqual({message: 'Cannot parse passed in authorization token'});
 
-//mockResponse.json toBe
+  })
+
+  it('Should call status 401 due to incorrect token', () => {
+
+    //Valid auth token, but different userId
+    mockRequest = {
+      headers: {authorization: 'valid'},
+      params: {userId: '2'}
+  }
+    restricted(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(resultStatus).toBe(401);
+    expect(resultJson).toEqual({message: 'You are not authorized to view this information'});
+
+  })
+
+  it('Should call status 400 due to missing auth token', () => {
+
+    mockRequest = {
+      headers: {},
+      params: {userId: '2'}
+    }
+
+    restricted(mockRequest as Request, mockResponse as Response, nextFunction);
+
+    expect(resultStatus).toBe(400);
+    expect(resultJson).toEqual({message: 'Bad request, please provide token in authorization headers'});
+
+  })
+});
