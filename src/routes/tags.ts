dd17fc1e29548345d924c4409054b2e66aa1a903
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2021-12-08 22:43:28 -0800

    wrote post route but not completely working

--- src/routes/tags.ts
+++ src/routes/tags.ts
@@ -1,10 +1,35 @@
 import { Router } from 'express';
 import { restricted } from '../middleware/auth';
 
+import { User } from '../typeorm/entities/User';
+import { Task } from '../typeorm/entities/Task';
+import { Tag } from '../typeorm/entities/Tag';
+
 const router = Router();
 
 router.post('/:userId', restricted, async (req, res) => {
+
+  try {
+  const { userId } = req.params;
+  const user = await User.findOne({ id: userId });
+  const body: Tag = req.body;
+  const tag = new Tag();
+
+  if (user) {
+
+    tag.tagName = body.tagName;
+    tag.tagColor = body.tagColor;
   
+    await tag.save();
+    res.status(201).send();
+
+  } else {
+    res.status(404).json({ message: 'Not Found' });
+  }
+} catch (err) {
+  res.status(400).send();
+}
+
 });
 
 
