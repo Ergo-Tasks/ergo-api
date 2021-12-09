937780fd52bbe48005b1f7f3692c594a35280a69
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2021-12-08 22:42:58 -0800

    5 new tests for tag post route

--- src/routes/__tests__/tags.test.ts
+++ src/routes/__tests__/tags.test.ts
@@ -5,7 +5,7 @@ import server from "../../server";
 import createConnection from '../../typeorm';
 import { Tag } from '../../typeorm/entities/Tag';
 import { User } from '../../typeorm/entities/User';
-import color from 'color';
+import { Task } from '../../typeorm/entities/Task';
 
 jest.mock('../../middleware/auth', () => ({
   restricted: (req: Request, res: Response, nextFunction: NextFunction) => {
@@ -19,7 +19,14 @@ describe('Tag routes', () => {
 
   const tagExample = {
     tagName: 'workout',
-    tagColor: color.rgb(255,255,255)
+    tagColor: '#fff'
+  }
+
+  const taskExample = {
+    taskName: 'Econ Work 2',
+    taskDescription: 'Complete problems 13-54 in textbook Ch. 12',
+    isRecursive: false,
+    taskDate: 1628912941, // 8-13-21 20-49-01
   }
 
   const userExample = {
@@ -31,6 +38,7 @@ describe('Tag routes', () => {
   }
 
   let dbUser:User;
+  let dbTask:Task;
 
   beforeAll(async () => {
     await createConnection();
@@ -41,14 +49,58 @@ describe('Tag routes', () => {
 
   describe('POST /api/tags/:userId', () => {
 
-    it('Should return status 201', async () => {
+    it('Should return status 201 (without taskId)', async () => {
       const res = await request.post(`/api/tags/${dbUser.id}`)
         .send(tagExample);
       
       expect(res.status).toBe(201);
     });
-
     
+    it('Should return status 201 (with taskId) and link task with tag using taskId', async () => {
+      const res = await request.post(`/api/tags/${dbUser.id}/${dbTask.id}`)
+      .send(tagExample);
+      const expectedResponse = JSON.stringify(dbTask.tags);
+      
+      expect(res.status).toBe(201);
+      expect(res.text).toBe(expectedResponse);
+    });
+    
+    it('Should return status 400 because of missing tagName field', async () => {
+      const res = await request.post(`/api/tags/${dbUser.id}`)
+        .send({...tagExample, tagName: ''});
+      const expectedResponse = JSON.stringify({ message: 'Bad Request: Missing Required Field' });
+
+
+      expect(res.status).toBe(400);
+      expect(res.text).toBe(expectedResponse);
+    });
+
+    it('Should return status 400 because of missing tagColor field', async () => {
+      const res = await request.post(`/api/tags/${dbUser.id}`)
+        .send({...tagExample, tagColor: ''});
+      const expectedResponse = JSON.stringify({ message: 'Bad Request: Missing Required Field' });
+
+      expect(res.status).toBe(400);
+      expect(res.text).toBe(expectedResponse);
+    });
+
+    it('Should return status 404 (with taskId) because of invaild taskId', async () => {
+      const res = await request.post(`/api/tags/${dbUser.id}/invalidTaskId`)
+        .send(tagExample);
+      const expectedResponse = JSON.stringify({ message: 'Not Found' });  
+
+      expect(res.status).toBe(404);
+      expect(res.text).toBe(expectedResponse);
+    });
+
+    it('Should return status 404 (without taskId) because of invalid userId', async () => {
+      const res = await request.post('/api/tags/invalidUserId')
+        .send(tagExample);
+      const expectedResponse = JSON.stringify({ message: 'Not Found' });  
+
+      expect(res.status).toBe(404);
+      expect(res.text).toBe(expectedResponse);
+    });
 
   });
 
