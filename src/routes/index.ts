1146e6ee43cc623fa87f1a377087e6ac25b09b00
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-01-07 17:07:25 -0800

    applied tag route to express server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 
 import userRoute from "./users";
 import taskRoute from "./tasks";
+import tagRoute from './tags';
 
 /** 
  * Handles applying all routes to given server
@@ -12,6 +13,7 @@ import taskRoute from "./tasks";
 const applyRoutes = (server: Express) => {
   server.use("/api/users", userRoute);
   server.use("/api/tasks", taskRoute);
+  server.use('/api/tags', tagRoute)
   server.get("/", (req, res) => {res.send("Ergo api running here")});
 }
 
