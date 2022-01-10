631ed147a751ac017026520263aa778df7876fa8
reevesabbas <reeves.abbas@gmail.com>
2022-01-10 13:35:10 -0500

    added 2 get routes, put and delete route

--- src/routes/tags.ts
+++ src/routes/tags.ts
@@ -28,4 +28,59 @@ router.post('/:userId', restricted, async (req, res) => {
 
 });
 
+//returns all user's tags
+router.get('/:userId', restricted, async (req, res) => {
+
+  //i don't think there is a way to do this without relating user and tag by one to many
+
+});
+
+//Returns a tag found by tagId in params
+router.get('/:tagId', restricted, async (req, res) => {
+
+  const { tagId } = req.params;
+  const tag = await Tag.findOne({ id: tagId });
+  
+  if (tag) res.status(200).json({tag})
+  else res.status(404).json({message: 'Not Found'})
+
+})
+
+router.put('/:tagId', restricted, async (req, res) => {
+
+  try {
+
+    const { tagId } = req.params;
+    const tag = await User.findOne({ id: tagId });
+    const body: Tag = req.body;
+    
+    if(tag) {
+      Object.assign(tag, {
+        ...body
+      })
+      
+      await tag.save();
+      res.status(200).json({tag});
+    }
+
+  } catch(err) {
+    res.status(400).json({message: 'Bad Request', error: err})
+  }
+  
+})
+
+router.delete('/:tagId', restricted, async (req, res) => {
+
+  const { tagId } = req.params;
+  const tag = await User.findOne({ id: tagId });
+
+  if (tag) {
+    await tag.remove();
+    res.status(201).send();
+  } else {
+    res.status(404).json({message: 'Not Found'})
+  }
+  
+})
+
 export default router;
