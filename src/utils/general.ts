03d460f74052bc4c668f550bb7dc882f944b322f
kaydengrant <74332434+kaydengrant@users.noreply.github.com>
2022-02-16 15:52:32 -0800

    util file for creating test data

---
+++ src/utils/general.ts
@@ -0,0 +1,19 @@
+import { Tag } from "../typeorm/entities/Tag";
+import { User } from "../typeorm/entities/User";
+
+export const createTestUser = async (email: string = 'defaultEmail@gmail.com') => {
+  const testUser = new User();
+  testUser.userName = 'Marty372';
+  testUser.firstName = 'Marty';
+  testUser.lastName = 'Byrde';
+  testUser.email = email;
+  testUser.password = 'passwordlol';
+  return await testUser.save();
+}
+
+export const createTestTag = async (tagName: string = 'defaultTagName') => {
+  const testTag = new Tag();
+  testTag.tagName = tagName;
+  testTag.tagColor = 'Blue'
+  return await testTag.save();
+}
